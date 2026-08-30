d6b99dc4d4eed014caadc7436ae59a166bca5fb7
Nicholas <nickmathew810@gmail.com>
2026-08-30 14:57:08 -0500

    Init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+	const forceLock = async () => {
+		try {
+			await document.documentElement.requestFullscreen();
+		} catch (error) {
+			console.log(error);
+		}
+	};
+</script>
+
+<h1>Serious About Work</h1>
+
+<button onclick={forceLock}>start session</button>
